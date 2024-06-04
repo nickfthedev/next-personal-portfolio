533094455f92300cd37fb8792f50ccbd2e8ebf5d
Nick Friedrich <nfriedrich1990@gmail.com>
2024-06-04 16:48:14 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,102 @@
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 import Image from "next/image";
+
+export const pageInfo = {
+  pageName: "Personal Portfolio of Max Mustermann",
+  pageOwner: "Max Mustermann",
+  currentYear: new Date().getFullYear().toString(),
+  githubProfile: "https://www.github.com/nextjs",
+};
+
+interface projectInterface {
+  id: number;
+  name: string;
+  year: string;
+  status: string;
+  logo: string;
+  description: string;
+}
+
+const projects: projectInterface[] = [
+  {
+    id: 1,
+    name: "Windows 95",
+    year: "1995",
+    status: "LIVE",
+    logo: "", // Put logo in public folder and use filename here (without public)
+    description: "Yes, that was me (not)",
+  },
+  {
+    id: 2,
+    name: "Windows 98",
+    year: "1998",
+    status: "STILL LIVE",
+    logo: "",
+    description: "Guilty",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex flex-col justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl tracking-tighter font-bold bg-clip-text text-transparent bg-gradient-to-r from-sky-600 to-sky-600 via-blue-300">
+          {pageInfo.pageOwner}
+        </h1>
+        <p className="text-xl tracking-wider text-gray-400 font-semibold ">
+          Personal Portfolio
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <Link href={pageInfo.githubProfile} target="_blank">
+          <button className="mt-8 inline-flex items-center px-6 py-3 bg-gray-200 text-black text-sm rounded-lg hover:bg-gray-300">
+            <FaGithub size={24} className="mr-2" />
+            Visit my Github
+          </button>
+        </Link>
+      </div>
+      <div className="w-full mx-auto flex flex-wrap justify-center mt-10 gap-2 max-w-4xl">
+        <div>
+          <h2 className="text-xl font-semibold tracking-tighter text-gray-600">
+            Current Projects
+          </h2>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {projects.map((project) => (
+          <div key={project.id} className="w-full">
+            <div className="bg-gray-100 border border-gray-200 rounded-lg p-6 flex flex-row items-center text-left">
+              <div className="flex items-center justify-center w-24 h-24 rounded-full shadow-md">
+                {project.logo ? (
+                  <Image
+                    src={project.logo}
+                    alt={`${project.name} logo`}
+                    width={64}
+                    height={64}
+                    className="rounded-full"
+                  />
+                ) : (
+                  <div className="w-full h-full rounded-full bg-gray-400"></div>
+                )}
+              </div>
+              <div className="mx-5">
+                <div>
+                  <h2 className="text-lg font-bold">{project.name}</h2>
+                  <p className="text-gray-600">
+                    Started in: {project.year} - Status:{" "}
+                    <span
+                      className={`${
+                        project.status === "LIVE"
+                          ? "text-green-500 font-semibold"
+                          : "text-gray-500 font-semibold"
+                      }`}
+                    >
+                      {project.status}
+                    </span>
+                  </p>
+                </div>
+                <p className="text-gray-700 mt-2">{project.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
